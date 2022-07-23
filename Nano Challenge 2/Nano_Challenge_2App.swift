34378606d073657f8d629eae2a0a6365a0d3ed8a
Adam Fiadi <adamfiadi@gmail.com>
2022-07-23 17:44:44 +0700

    Initial Commit

---
+++ Nano Challenge 2/Nano_Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge_2App.swift
+//  Nano Challenge 2
+//
+//  Created by Adam Ibnu fiadi on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
